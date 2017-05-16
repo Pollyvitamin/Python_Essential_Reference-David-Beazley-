7b753fcd184b1b1d98adcc0c6bc679266565291b
Pollyvitamin <Fedorovichps@gmail.com>
2017-05-16 09:12:56 +0300

    Create 'tail -f | grep python'.py
    Implementation of the command sequence “tail -f | grep python”

---
+++ 'tail -f | grep python'.py
@@ -0,0 +1,20 @@
+# следит за содержимым файла (на манер команды tail -f)
+import time
+def tail(f):
+    f.seek(0,2) # Переход в конец файла
+    while True:
+        line = f.readline() # Попытаться прочитать новую строку текста
+        if not line: # Если ничего не прочитано,
+            time.sleep(0.1) # приостановиться на короткое время
+            continue # и повторить попытку
+        yield line
+
+def grep(lines, searchtext):
+    for line in lines:
+        if searchtext in line: yield line
+
+# Реализация последовательности команд “tail -f | grep python”
+wwwlog = tail(open(“access-log”))
+pylines = grep(wwwlog,”python”)
+for line in pylines:
+    print line,
