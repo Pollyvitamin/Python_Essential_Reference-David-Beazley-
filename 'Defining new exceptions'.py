26f51753b17525f131093e6f811b6e618a14c0a6
Pollyvitamin <Fedorovichps@gmail.com>
2017-05-22 11:10:44 +0300

    Create 'Defining new exceptions'.py

---
+++ 'Defining new exceptions'.py
@@ -0,0 +1,28 @@
+class NetworkError(Exception): # наследует класс Exception
+	pass 
+raise NetworkError('Невозможно найти компьютер в сети.')
+
+class HostnameError(NetworkError): 
+	pass
+class TimeoutError(NetworkError): 
+	pass
+
+def error1():
+	raise HostnameError('Хост не найден')
+def error2():
+	raise TimeoutError('Превышено время ожидания')
+try:
+	error1()
+except NetworkError as e:
+	if type(e) is HostnameError:
+		# Выполнить действия, характерные для ошибки этого типа
+		...
+
+class DeviceError(Exception):
+	def __init__(self,errno,msg):
+		self.args = (errno, msg)
+		self.errno = errno
+		self.errmsg = msg
+		
+# Возбудить исключение (передав несколько аргументов)
+raise DeviceError(1, 'Нет ответа')
